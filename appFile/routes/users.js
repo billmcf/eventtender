bef7417953f1c463ac8102386929180c963d659a
Kashi Rana Halma <kashi.halma@gmail.com>
2017-10-07 16:10:19 -0600

    hey dudes i made some changes

--- appFile/routes/users.js
+++ appFile/routes/users.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var router = express.Router();
-
+const boom= require('boom')
+var emailReg=/@/;
 /* GET users listing. */
-router.post('/', function(req, res, next) {
+//var {first_name}=req.body
+router.post('/', function(req, res, next, err) {
+  //console.log(req.body)
+  // if(!req.body.first_name||!req.body.first_name.trim()){
+  //   return next(boom.create(400, 'please provide a first name'));
+  // }
+  // if(!req.body.last_name||!req.body.last_name.trim()){
+  //   return next(boom.create(400, 'please provide a first and last name'));
+  // }
+  // if(!req.body.email||!req.body.email.trim()||emailReg.test(req.body.email===false)){
+  //   return next(boom.create(400, 'please provide a valid email'));
+  // }
+  // if(!req.body.password){
+  //   return next(boom.create(400, 'please provide a password'));
+  // }
   res.send('users post a go go');
 });
 
