42a2bfc6b1d8d5b46c234e8827083adeced91e80
Kashi Rana Halma <kashi.halma@gmail.com>
2017-10-08 12:38:51 -0600

    Changed up the seeds

--- appFile/seeds/1_users.js
+++ appFile/seeds/1_users.js
@@ -6,7 +6,8 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('users').insert([
         {id: 1, is_admin: true, email: 'runbmc@gmail.com', password: 'admin'},
-        {id: 2, email: 'beef.deluxe@gmail.com', password: 'beefhouse'}
+        {id: 2, email: 'beef.deluxe@gmail.com', password: 'beefhouse'},
+        {id: 3, email: 'chicken.parm@gmail.com', password: 'chickenparm'}
       ]);
     });
 };
--- appFile/seeds/2_events.js
+++ appFile/seeds/2_events.js
@@ -5,7 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('events').insert([
-        {id: 1, name: 'Landing Page', date:'1992-08-05', user_id:1}
+        {id: 1, name: 'Landing Page', date:'1992-08-05', user_id:1},
+        {id: 2, name: 'Beef the World Behind', date:'1982-08-05', user_id:2},
+        {id: 3, name: 'Chicken Parm Convention', date:'1972-08-05', user_id:3},
       ]);
     });
 };
