9375dc55a6e1514bc36a10e18607f0050427215e
Kashi Rana Halma <kashi.halma@gmail.com>
2017-10-08 16:04:43 -0600

    adjusting our drink router

--- appFile/routes/drinks.js
+++ appFile/routes/drinks.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var knex= require('../knex')
 var router = express.Router();
+const boom=require('boom')
 
-
-router.get('/', function(req, res, next) {
-  res.send('drinks get');
+router.get('/:id', function(req, res, next) {
+  if(Object.keys(req.cookies).length===0){
+    return next(boom.create(400, 'user access only'));
+  }
+  knex('events').where({id: req.params.id}).then((event)=>{
+    if(event.length===0){
+      return next(boom.create(400,"Event does not exist"))
+    }
+    knex('drinks').where({event_id: req.params.id}).then((drinks)=>{
+      if(drinks.length===0){
+        return res.send('No drinks have been saved for this event, please add some!')
+      }
+      res.send(drinks);
+    })
+  })
 });
 router.post('/', (req, res, next)=>{
   res.send('drinks post')
