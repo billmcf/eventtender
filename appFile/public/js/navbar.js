e9a75aa625bdeb0bd64198aa3fd2adf8df61d4c1
Kashi Rana Halma <kashi.halma@gmail.com>
2017-10-09 11:19:07 -0600

    nav bar a go go

--- appFile/public/js/navbar.js
+++ appFile/public/js/navbar.js
@@ -4,34 +4,103 @@ var login='http://localhost:3000/login.html'
 var signup='http://localhost:3000/signup.html'
 var userPage='http://localhost:3000/user.html'
 
+if(window.location.href===landing){
 var nav=$('#nav');
 
 var outerDiv=document.createElement('div');
-outerDiv.setAttribute('class', 'nav-wrapper');
+outerDiv.setAttribute('class', "nav-wrapper");
 var outerLink=document.createElement('a');
-outerLink.setAttribute('class', 'brand-logo');
-//outerLink.innerHTML='Event Tender';
+outerLink.setAttribute('class', "brand-logo");
+outerLink.innerHTML='Event Tender';
 var list=document.createElement('ul');
-list.setAttribute('class', 'right hide-on-med-and-down');
+list.setAttribute('class', "right hide-on-med-and-down");
 var listItem1=document.createElement('li');
 var listItem2=document.createElement('li');
 var listItem3=document.createElement('li');
 var listItemLink1=document.createElement('a');
-listItemLink1.setAttribute('class', 'waves-effect waves-light btn')
+listItemLink1.setAttribute('class', "waves-effect waves-light btn")
+listItemLink1.setAttribute('href','http://localhost:3000/signup.html' )
 listItemLink1.innerHTML='Signup'
 var listItemLink2=document.createElement('a');
-listItemLink2.setAttribute('class', 'waves-effect waves-light btn')
+listItemLink2.setAttribute('class', "waves-effect waves-light btn")
+listItemLink2.setAttribute('href','http://localhost:3000/login.html' )
 listItemLink2.innerHTML='Login'
 var listItemLink3=document.createElement('a');
-listItemLink3.setAttribute('class', 'waves-effect waves-light btn')
-listItemLink3.innerHTML='Logout'
+listItemLink3.setAttribute('class', "waves-effect waves-light btn")
 listItem1.append(listItemLink1)
 listItem2.append(listItemLink2)
-listItem3.append(listItemLink3)
 list.append(listItem1)
 list.append(listItem2)
-list.append(listItem3)
 outerLink.append(list);
 outerDiv.append(outerLink)
 nav.append(outerDiv)
+}
+else if(window.location.href===login){
+var nav=$('#nav');
+var outerDiv=document.createElement('div');
+outerDiv.setAttribute('class', "nav-wrapper");
+var outerLink=document.createElement('a');
+outerLink.setAttribute('class', "brand-logo");
+outerLink.innerHTML='Event Tender';
+var list=document.createElement('ul');
+list.setAttribute('class', "right hide-on-med-and-down");
+var listItem1=document.createElement('li');
+var listItemLink1=document.createElement('a');
+listItemLink1.setAttribute('class', "waves-effect waves-light btn")
+listItemLink1.setAttribute('href','http://localhost:3000' )
+listItemLink1.innerHTML='Home'
+listItem1.append(listItemLink1)
+list.append(listItem1)
+outerLink.append(list);
+outerDiv.append(outerLink)
+nav.append(outerDiv)
+}
+if(window.location.href===signup){
+  var nav=$('#nav');
+  var outerDiv=document.createElement('div');
+  outerDiv.setAttribute('class', "nav-wrapper");
+  var outerLink=document.createElement('a');
+  outerLink.setAttribute('class', "brand-logo");
+  outerLink.innerHTML='Event Tender';
+  var list=document.createElement('ul');
+  list.setAttribute('class', "right hide-on-med-and-down");
+  var listItem1=document.createElement('li');
+  var listItemLink1=document.createElement('a');
+  listItemLink1.setAttribute('class', "waves-effect waves-light btn")
+  listItemLink1.setAttribute('href','http://localhost:3000' )
+  listItemLink1.innerHTML='Home'
+  listItem1.append(listItemLink1)
+  list.append(listItem1)
+  outerLink.append(list);
+  outerDiv.append(outerLink)
+  nav.append(outerDiv)
+}
+if(window.location.href===userPage){
+var nav=$('#nav');
+
+var outerDiv=document.createElement('div');
+outerDiv.setAttribute('class', "nav-wrapper");
+var outerLink=document.createElement('a');
+outerLink.setAttribute('class', "brand-logo");
+outerLink.innerHTML='Event Tender';
+var list=document.createElement('ul');
+list.setAttribute('class', "right hide-on-med-and-down");
+var listItem1=document.createElement('li');
+var listItem2=document.createElement('li');
+var listItemLink1=document.createElement('a');
+listItemLink1.setAttribute('class', "waves-effect waves-light btn")
+listItemLink1.setAttribute('href','http://localhost:3000' )
+listItemLink1.innerHTML='Home'
+var listItemLink2=document.createElement('a');
+listItemLink2.setAttribute('class', "waves-effect waves-light btn")
+listItemLink2.setAttribute('href','http://localhost:3000' )
+listItemLink2.innerHTML='Logout'
+listItem1.append(listItemLink1)
+listItem2.append(listItemLink2)
+list.append(listItem1)
+list.append(listItem2)
+outerLink.append(list);
+outerDiv.append(outerLink)
+nav.append(outerDiv)
+}
 })
