7b1380c2279dc4c0ae7e74db203022585b8f8db6
Kashi Rana Halma <kashi.halma@gmail.com>
2017-10-08 18:47:13 -0600

    good progress dudes

--- appFile/routes/drinks.js
+++ appFile/routes/drinks.js
@@ -2,6 +2,8 @@ var express = require('express');
 var knex= require('../knex')
 var router = express.Router();
 const boom=require('boom')
+const jwt= require('jsonwebtoken')
+var drinkPing=0;
 
 router.get('/:id', function(req, res, next) {
   if(Object.keys(req.cookies).length===0){
@@ -47,14 +49,59 @@ router.delete('/', (req, res, next)=>{
   if(Object.keys(req.cookies).length===0){
     return next(boom.create(400, 'user access only'));
   }
-  if(!req.body.eventId){
+  if(!req.body.event_id){
     return next(boom.create(400, 'Please enter and event ID'));
   }
-  const eventId = Number.parseInt(req.body.eventId);
-  if (!Number.isInteger(eventId)) {
+  const event_id = Number.parseInt(req.body.event_id);
+  if (!Number.isInteger(event_id)) {
     return next(boom.create(400, 'Event ID must be an integer'));
 }
+if(!req.body.drink_id){
+  return next(boom.create(400, 'Please select a drink to delete'));
+}
+const drink_id = Number.parseInt(req.body.drink_id);
+if (!Number.isInteger(drink_id)) {
+  return next(boom.create(400, 'Drink ID must be an integer'));
+}
   var reveal= jwt.verify(req.cookies.token, process.env.SECRET_KEY)
+
+  knex('events').select('id').where({id: event_id}).then((event)=>{
+    if(event.length===0){
+      return next(boom.create(400, 'event does not exist'))
+    }
+  knex('events').select('id').where({user_id: reveal}).then((id)=>{
+    if(id.length===0){
+      return next(boom.create(400, 'You have no events to delete drinks from.'))
+    }
+    var trackerPing=0;
+    for(var i=0; i<id.length; i++){
+      if(id[i]['id']===event_id){
+        trackerPing++;
+      }
+    }
+    if(trackerPing===0){
+      return next(boom.create(400, 'Event is not associated with your account'))
+    }
+    knex('drinks').select('id').where({event_id: event_id}).then((drink)=>{
+      if(drink.length===0){
+        return next(boom.create(400, 'Event has no drinks'))
+      }
+      for(var i=0; i<drink.length; i++){
+        if(drink_id===drink[i]['id']){
+          drinkPing++;
+        }
+      }
+      if(drinkPing===0){
+        return next(boom.create(400, 'drink not associated with event'))
+      }
+      knex('drinks').delete().where({id: drink_id}).then(()=>{
+        res.send('drink has been removed')
+      })
+      })
+    })
+  })
+
+
 })
 
 module.exports = router;
